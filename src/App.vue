6a29cfae949112a339353e7125cf809707c975ca
nihad-hh <hodzicnihad10@gmail.com>
2024-02-13 13:48:49 +0100

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/ui-parts/Header.vue'
+import Footer from './components/ui-parts/Footer.vue'
 </script>
 
 <template>
   <div>
     <Header />
-
     <RouterView />
+    <Footer />
   </div>
 </template>
---
+++ src/components/ui-parts/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="bg-white shadow dark:bg-gray-900 m-4">
+    <div class="w-full mx-auto p-4 md:py-8">
+      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span class="block text-sm text-gray-500 text-center dark:text-gray-400"
+        >© Book Finder 2023 All Rights Reserved.</span
+      >
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
